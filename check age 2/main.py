3743b2aee2da3e7028e2241ad330b2e5b5687412
tejasvi4235 <mani.chaurasiya@gmail.com>
2025-04-13 20:21:35 +0530

    Add files via upload

---
+++ check age 2/main.py
@@ -0,0 +1,8 @@
+#this project is based on nested loops
+age = int(input("enter your age"))
+if age > 10 :
+    print("you are above 10 years old so you can play free fire")
+if age < 10 :
+    print("sorry you can't play free fire but you can play minecraft")
+else:
+    print("you can play sonic forces")
